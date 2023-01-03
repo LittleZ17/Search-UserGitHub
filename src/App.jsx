d7a2c020824e9b37ea1f5161e7daaafbdbb6f940
Little Z <zy_mz17@hotmail.es>
2023-01-03 22:55:05 +0100

    search function ok

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,24 @@ import Search from './components/Search/Search'
 
 function App() {
 
-  const [ userState, setUserState ] = useState('octocat');
-  const [ inputUser, setInputUser ] = useState(userState);
+  const [userState, setUserState] = useState('octocat');
+  const [inputUser, setInputUser] = useState(userState);
+  // console.log('despues del click', inputUser)
 
 
   return (
-    <Container sx={{
-      background: 'whitesmoke',
-      width: '80vw',
-      height: '500px',
-      borderRadius:'16px',
-      marginTop:'40px',
-      display:'flex',
-      flexDirection:'column',
-      alignItems:'center'
-    }}>
-      <Search inputUser={inputUser} setInputUser={setInputUser}/>
+    <Container
+      sx={{
+        background: 'whitesmoke',
+        width: '80vw',
+        height: '500px',
+        borderRadius: '16px',
+        marginTop: '40px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}>
+      <Search inputUser={inputUser} setInputUser={setInputUser} />
     </Container>
   )
 }
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IconButton, Stack, TextField } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search'
 
-const Search = () => {
+const Search = (props) => {
 
-    const handleSubmit = (props) => {
+    const { setInputUser } = props;
 
+    const [valueInput, setValueInput] = useState('')
+
+    const onSearchValueChange = (event) => {
+        const inputValue = event.target.value;
+        setValueInput(inputValue)
     }
+
+    // console.log('valor inicial', valueInput)
+
+    const handleSubmit = () => {
+        setInputUser(valueInput);
+    }
+
 
     return (
 
@@ -22,17 +34,19 @@ const Search = () => {
                 placeholder="Octocat"
                 variant="outlined"
                 size='small'
+                value={valueInput}
+                onChange={onSearchValueChange}
                 sx={{
                     width: '90%',
                 }}></TextField>
             <IconButton
                 onClick={handleSubmit}
                 sx={{
-                    left:'-45px'
+                    left: '-45px'
                 }}>
                 <SearchIcon />
             </IconButton>
-            
+
         </Stack>
     )
 }
